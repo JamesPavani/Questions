f0062d573cb805c0d8444d9f31bb0bda0107be79
Thiago Pavani <tsantanna05@gmail.com>
2022-07-01 00:08:09 -0300

    First Commit

---
+++ TransfererProgram.py
@@ -0,0 +1,44 @@
+from time import sleep
+from plyer import notification
+from maskpass import askpass
+
+senha_real = 749
+
+print("---------------------------------")
+sleep(1)
+print("|             SENHA             |")
+sleep(1)
+print("---------------------------------")
+sleep(2)
+senha_digitada = int(askpass(prompt = "|Digite a Senha: ", mask = "*"))
+sleep(2)
+print("---------------------------------")
+sleep(1)
+print("|                               |")
+sleep(1)
+print("---------------------------------")
+sleep(1)
+
+print(" ")
+sleep(2)
+
+if senha_digitada == senha_real:
+    print("---------------------------------")
+    sleep(1)
+    print("|", "\033[1;32m         SENHA ACEITA\033[m", "        |")
+    sleep(1)
+    print("---------------------------------")
+
+    notification.notify(
+        title = "Mensagem:",
+        message = "Bem-Vindo",
+        app_icon = "",
+        timeout = 10
+    )
+
+if senha_digitada != senha_real:
+    print("---------------------------------")
+    sleep(1)
+    print("|", "\033[1;31m         SENHA NEGADA\033[m", "        |")
+    sleep(1)
+    print("---------------------------------")
